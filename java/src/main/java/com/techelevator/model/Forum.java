cabb5f4a5aa27eac4d6bd55e60619f81aed4cecb
Jasir Gordon-Jones <Jfrog2003@gmail.com>
2024-09-30 10:47:30 -0400

    forums

---
+++ java/src/main/java/com/techelevator/model/Forum.java
@@ -0,0 +1,33 @@
+package com.techelevator.model;
+
+public class Forum {
+    private int id;
+
+    private String topic;
+
+    private String author;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
