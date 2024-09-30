e61ffb937d7178e61c8f9a1ddd1e2148480116c1
Jasir Gordon-Jones <Jfrog2003@gmail.com>
2024-09-30 11:45:01 -0400

    saving work

--- java/src/main/java/com/techelevator/controller/ForumController.java
+++ java/src/main/java/com/techelevator/controller/ForumController.java
@@ -1,6 +1,23 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.ForumsDao;
+import com.techelevator.model.Forum;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ForumController {
 
+    private ForumsDao forumsDao;
 
+    public ForumController(ForumsDao forumsDao) {
+        this.forumsDao = forumsDao;
+    }
+
+    @GetMapping("path: /forums")
+    public List<Forum> getListOfForums() {
+        return forumsDao.getForums();
+    }
 }
--- java/src/main/java/com/techelevator/dao/JdbcForumDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcForumDao.java
@@ -1,13 +1,43 @@
 package com.techelevator.dao;
 
+import com.techelevator.exception.DaoException;
 import com.techelevator.model.Forum;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcForumDao implements ForumsDao{
 
+    private JdbcTemplate jdbcTemplate;
+    public JdbcForumDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public List<Forum> getForums(){
-        
-        return null;
+        List<Forum> list = new ArrayList<>();
+        String sql = "SELECT * FROM forums";
+
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+            while (results.next()) {
+                Forum forum = mapRowToForum(results);
+                list.add(forum);
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        }
+        return list;
+    }
+
+    private Forum mapRowToForum(SqlRowSet rs) {
+        Forum forum = new Forum();
+        forum.setId(rs.getInt("forum_id"));
+        forum.setTopic(rs.getString("topic"));
+        forum.setAuthor(rs.getString("author"));
+        forum.setTimeOfCreation(rs.getTimestamp("time_of_creation"));
+        return forum;
     }
 }
