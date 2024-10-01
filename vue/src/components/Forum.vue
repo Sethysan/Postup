e350a1b74776509cc41cb72f3c454948dc452219
Jennifer Forse <forsejennyb@gmail.com>
2024-10-01 09:41:48 -0500

    pnkjasd

---
+++ vue/src/components/Forum.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="forum">
+        <h1>{{ forum.topic }}</h1>
+        <p>{{ forum.description }}</p>
+    </div>
+</template>
+
+<script>
+import service from '../services/ForumService'
+
+export default {
+    props: ['form'],
+    data(){
+        return {
+            currentForum: {}
+        }
+    },
+    created(){
+        this.currentForum = this.forum ? this.forum : service.getForum(this.$route.params.forum).then(res => this.currentForum = res.data);
+    }
+}
+</script>
+
+<style>
+
+</style>
