826a3c39ce84c7b4d180e98093c1dedd669de0d3
Mitchell Wong <mitch10wong@gmail.com>
2024-10-02 10:03:55 -0400

    Forum Link Chnage

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -2,7 +2,6 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
-    <router-link v-bind:to="{ name: 'forums' }">Forums</router-link>
     <h1>todays top 10 popular posts</h1>
     <post-list></post-list>
   </div>
