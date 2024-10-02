b23267cd40cf1f6f9cc499f07733cbf6bba9157c
Mitchell Wong <mitch10wong@gmail.com>
2024-10-02 10:38:37 -0400

    Login button shows when logged out

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,6 +4,7 @@
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|
       <router-link v-bind:to="{ name: 'forums' }">Forums</router-link> |
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>
     </div>
     <router-view />
   </div>
