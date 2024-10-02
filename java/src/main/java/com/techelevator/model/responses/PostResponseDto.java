33457648fbc2aab98e47802cc6ae4e50223ccb04
Jennifer Forse <forsejennyb@gmail.com>
2024-10-02 08:27:53 -0500

    yup

--- java/src/main/java/com/techelevator/model/responses/PostResponseDto.java
+++ java/src/main/java/com/techelevator/model/responses/PostResponseDto.java
@@ -9,15 +9,6 @@ public class PostResponseDto {
     private int upvotes;
     private int downvotes;
     private String creator_username;
-    private String title;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
     public long getId() {
         return id;
