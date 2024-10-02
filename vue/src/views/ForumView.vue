a330c622bb2efbdd5cbed444c2d1aefe4ca45351
Jennifer Forse <forsejennyb@gmail.com>
2024-10-02 13:09:14 -0500

    fixed post-list and moved fetching functionality up into the parents

--- vue/src/views/ForumView.vue
+++ vue/src/views/ForumView.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <forum :forum="forum"></forum>
-    <post-list :forum="forumId"></post-list>
+    <post-list :posts="posts"></post-list>
   </div>
 </template>
 
 <script>
 import PostList from '../components/PostList.vue';
 import Forum from '../components/Forum.vue';
-import service from '../services/ForumService'
+import ForumService from '../services/ForumService'
+import PostService from '../services/PostService';
 
 export default {
   components: {
@@ -18,23 +19,34 @@ export default {
   data() {
     return {
       forum: {},
-      forumId: 0
+      forumId: 0,
+      posts: []
     }
   },
   created() {
     // Check if the URL contains the query parameter 'topic'
     if (this.$route.query.topic) {
       // Fetch forums by the topic query parameter
-      service.getForumTopic(this.$route.query.topic).then(res => {
-        this.forum = res.data;
+      ForumService.getForumTopic(this.$route.query.topic)
+        .then(res => {
+          this.forum = res.data;
+          PostService.getForumPosts(this.forumId)
+          .then(res => {
+            this.posts = res.data;
+          })
       }).catch((error) => {
         console.error("Error fetching forums by topic: ", error);
       });
-    } else {
+    } 
+    else {
       // Fetch forum by ID if 'topic' is not provided
       this.forumId = this.$route.params.id;
-      service.getForum(this.forumId).then(res => {
+      ForumService.getForum(this.forumId).then(res => {
         this.forum = res.data;
+        PostService.getForumPosts(this.forumId)
+          .then(res => {
+            this.posts = res.data;
+          })
       }).catch((error) => {
         console.error("Error fetching forum by ID: ", error);
       });
