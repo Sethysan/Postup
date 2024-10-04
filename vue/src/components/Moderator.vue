aded7d56c3ddd98b99c44187bf1303f4404ec838
Mitchell Wong <mitch10wong@gmail.com>
2024-10-04 11:55:40 -0400

    Mod Promote

--- vue/src/components/Moderator.vue
+++ vue/src/components/Moderator.vue
@@ -3,10 +3,27 @@
     <h1>Moderator</h1>
     <p>Welcome, Moderator!</p>
     <!-- for loop to list all usernames where role is equal to user -->
-    <!-- make the usernames a table <tr>-->
-    <!-- also inside loop button for promote to moderator -->
-    <!-- will also be a table component <td> -->
     <!-- only show on forums page and if role is mod or admin -->
+    <table v-if="canPromote && filteredUsers.length > 0">
+        <thead>
+            <!-- make the usernames a table <tr>-->
+            <tr>
+                <th>Username</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="user in filterUsers" :key="user.id">
+                <td>{{ user.username }}</td>
+                <!-- also inside loop button for promote to moderator -->
+                <!-- will also be a table component <td> -->
+                <td>
+                    <button @click="promoteUser(user.id)">Promote to Moderator</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <p v-else>No users to promote.</p>
   </div>
 </template>
 
@@ -17,6 +34,37 @@ export default {
         users: {
             type: Array,
             required: true
+        },
+        forumId: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+        canPromote() {
+            const isForumPath = this.$route.path.includes('/forums');
+            const isModOrAdmin = this.userRole === 'ROLE_MODERATOR' || this.userRole === 'ROLE_ADMIN';
+            return isForumPath && isModOrAdmin;
+        },
+        filteredUsers() {
+            return this.users.filter(user => user.role === 'ROLE_USER');
+        }
+    },
+    methods: {
+        promoteUser(userId) {
+            ModeratorService.promoteUser(userId, this.forumId)
+                .then(() => {
+                    // Update the user's role in the Vuex store
+                    this.$store.commit('PROMOTE_USER', userId);
+                })
+                .catch(error => {
+                    console.error('Error promoting user:', error);
+                });
+        }
+    },
+    data() {
+        return {
+            userRole: this.$store.getters.role
         }
     }
     // method to see if in forums path and if role is mod or admin canPromote
