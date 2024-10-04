c8a9c8950603ff2fdf20763fd85da64f8063316b
Mitchell Wong <mitch10wong@gmail.com>
2024-10-04 10:35:06 -0400

    Notes

--- vue/src/components/Moderator.vue
+++ vue/src/components/Moderator.vue
@@ -3,7 +3,9 @@
     <h1>Moderator</h1>
     <p>Welcome, Moderator!</p>
     <!-- for loop to list all usernames where role is equal to user -->
+    <!-- make the users a table <tr>-->
     <!-- also inside loop button for promote to moderator -->
+    <!-- will also be a table component <td> -->
     <!-- only show on forums page and if role is mod or admin -->
   </div>
 </template>
