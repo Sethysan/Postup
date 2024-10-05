fe55c2e03d06d428c0ab6ec9d03a32594a4b4409
Jennifer Forse <forsejennyb@gmail.com>
2024-10-05 09:52:33 -0500

    finally, she works!

--- vue/src/views/FavoritesView.vue
+++ vue/src/views/FavoritesView.vue
@@ -1,17 +1,23 @@
 <template>
-  
+    <div v-for="forum in favorites" :key="forum.id">
+        <forum-snippet :forum="forum"></forum-snippet>
+    </div>
 </template>
 
 <script>
+import ForumSnippet from '../components/ForumSnippet.vue';
 import service from '../services/ForumService';
 
 export default {
+  components: { ForumSnippet  },
     data(){
+        return{
         favorites: []
+        }
     },
     created(){
         service.getFavoriteForums()
-            .then(res => this.favorties = res.data)
+            .then(res => this.favorites = res.data)
             .catch(err => alert("error"))
     }
 }
