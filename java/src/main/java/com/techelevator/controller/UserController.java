253517035eeabc19749666e422b924329660cad4
Jasir Gordon-Jones <Jfrog2003@gmail.com>
2024-10-07 10:35:09 -0400

    pushing user paths

--- java/src/main/java/com/techelevator/controller/UserController.java
+++ java/src/main/java/com/techelevator/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.UserDao;
+import com.techelevator.model.Authority;
 import com.techelevator.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @CrossOrigin
@@ -20,5 +23,30 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return userDao.getUsers();
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/users/filtered")
+    public List<User> getUsersFiltered() {
+        return userDao.getUsersFiltered();
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PutMapping("/user/{id}/ban")
+    public void banUser(@PathVariable long id, Principal user) {
+        Set<Authority> roles = userDao.getUserByUsername(user.getName()).getAuthorities();
+        boolean isAdmin = false;
+
+        for (Authority role : roles) {
+            if (role.getName().equals("ROLE_ADMIN")) {
+                isAdmin = true;
+            }
+        }
+        if (isAdmin) {
+            userDao.banUser(id);
+        }
+        else {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "You do not have permission to ban this User");
+        }
     }
 }
