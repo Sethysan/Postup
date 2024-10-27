663c1614af52652987f581a6048e8c9607b4aa34
Sethysan <seth.r.drummage@gmail.com>
2024-10-27 14:13:16 -0400

    DB rename

--- java/database/user.sql
+++ java/database/user.sql
@@ -2,24 +2,24 @@
 -- This script creates the database users and grants them the necessary permissions
 -- ********************************************************************************
 
-CREATE USER final_capstone_owner
+CREATE USER PostUp_owner
 WITH PASSWORD 'finalcapstone';
 
 GRANT ALL
 ON ALL TABLES IN SCHEMA public
-TO final_capstone_owner;
+TO PostUp_owner;
 
 GRANT ALL
 ON ALL SEQUENCES IN SCHEMA public
-TO final_capstone_owner;
+TO PostUp_owner;
 
-CREATE USER final_capstone_app_user
+CREATE USER PostUp_app_user
 WITH PASSWORD 'finalcapstone';
 
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON ALL TABLES IN SCHEMA public
-TO final_capstone_app_user;
+TO PostUp_app_user;
 
 GRANT USAGE, SELECT
 ON ALL SEQUENCES IN SCHEMA public
-TO final_capstone_app_user;
+TO PostUp_app_user;
