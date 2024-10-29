47e9515ba2b94ad7872b92b1170b512ad993dad0
Sethysan <seth.r.drummage@gmail.com>
2024-10-29 10:57:29 -0400

    railway

--- java/database/create.sh
+++ java/database/create.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-export PGPASSWORD='postgres1'
+#export PGPASSWORD='postgres1'
+#BASEDIR=$(dirname $0)
+#DATABASE=final_capstone
+#psql -U postgres -f "$BASEDIR/dropdb.sql" &&
+#createdb -U postgres $DATABASE &&
+#psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
+#psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql" &&
+#psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql"
+
+#!/bin/bash
+export PGPASSWORD='ZBcPejPJFUyqqWVwbNWMZBqDZcqAHYzU'
 BASEDIR=$(dirname $0)
-DATABASE=PostUp
-psql -U postgres -f "$BASEDIR/dropdb.sql" &&
-createdb -U postgres $DATABASE &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql"
+DATABASE=railway
+psql -U postgres -h junction.proxy.rlwy.net -p 36725 -f "$BASEDIR/dropdb.sql" &&
+psql -U postgres -h junction.proxy.rlwy.net -p 36725 -d $DATABASE -f "$BASEDIR/schema.sql" &&
+psql -U postgres -h junction.proxy.rlwy.net -p 36725 -d $DATABASE -f "$BASEDIR/data.sql" &&
+psql -U postgres -h junction.proxy.rlwy.net -p 36725 -d $DATABASE -f "$BASEDIR/user.sql"
--- java/database/dropdb.sql
+++ java/database/dropdb.sql
@@ -5,9 +5,11 @@
 -- The following line terminates any active connections to the database so that it can be destroyed
 SELECT pg_terminate_backend(pid)
 FROM pg_stat_activity
-WHERE datname = 'PostUp';
+--WHERE datname = 'final_capstone';
+--DROP DATABASE final_capstone;
 
-DROP DATABASE final_capstone;
+WHERE datname = 'postgres';
+DROP DATABASE postgres;
 
-DROP USER postup_owner;
-DROP USER postup_app_user;
+DROP USER final_capstone_owner;
+DROP USER final_capstone_app_user;
